e2fda831e11b53fb7bfc630e0213e56c2a00bf41
atarutin <andrey.tarutin@lykke-business.ch>
2023-02-28 16:01:51 +0300

    fix(LT-4407): roll back messaging libraries update

--- src/Lykke.Snow.PriceAlerts/Modules/CqrsModule.cs
+++ src/Lykke.Snow.PriceAlerts/Modules/CqrsModule.cs
@@ -8,6 +8,7 @@ using Lykke.Cqrs.Configuration.Routing;
 using Lykke.Cqrs.Middleware.Logging;
 using Lykke.Messaging.Serialization;
 using Lykke.Snow.Common.Correlation.Cqrs;
+using Lykke.Snow.Common.Startup;
 using Lykke.Snow.Cqrs;
 using Lykke.Snow.PriceAlerts.Contract.Models.Events;
 using Lykke.Snow.PriceAlerts.Projections;
@@ -64,9 +65,9 @@ namespace Lykke.Snow.PriceAlerts.Modules
                 Uri = new Uri(_settings.ConnectionString, UriKind.Absolute)
             };
 
-            var loggerFactory = ctx.Resolve<ILoggerFactory>();
+            var log = new LykkeLoggerAdapter<CqrsModule>(ctx.Resolve<ILogger<CqrsModule>>());
             
-            var engine = new RabbitMqCqrsEngine(loggerFactory,
+            var engine = new RabbitMqCqrsEngine(log,
                 ctx.Resolve<IDependencyResolver>(),
                 new DefaultEndpointProvider(),
                 rabbitMqSettings.Endpoint.ToString(),
@@ -76,8 +77,8 @@ namespace Lykke.Snow.PriceAlerts.Modules
                 Register.DefaultEndpointResolver(rabbitMqConventionEndpointResolver),
                 RegisterDefaultRouting(),
                 RegisterContext(),
-                Register.CommandInterceptors(new DefaultCommandLoggingInterceptor(loggerFactory)),
-                Register.EventInterceptors(new DefaultEventLoggingInterceptor(loggerFactory)));
+                Register.CommandInterceptors(new DefaultCommandLoggingInterceptor(log)),
+                Register.EventInterceptors(new DefaultEventLoggingInterceptor(log)));
 
             var correlationManager = ctx.Resolve<CqrsCorrelationManager>();
             engine.SetWriteHeadersFunc(correlationManager.BuildCorrelationHeadersIfExists);
